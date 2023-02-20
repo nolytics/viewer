07f050507af4a10b44d754077f8b3c2612bee4f4
freitzzz <1160907@isep.ipp.pt>
2023-02-20 12:51:38 +0000

    setup ripple ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {}
-	},
-	plugins: []
+	darkMode: 'class',
+	plugins: [require('rippleui')]
 };
