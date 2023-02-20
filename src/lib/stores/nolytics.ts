f9f1e5f07cfba1f0788270b85f3d14a97014f2d7
freitzzz <1160907@isep.ipp.pt>
2023-02-20 15:50:08 +0000

    implement nolytics store

---
+++ src/lib/stores/nolytics.ts
@@ -0,0 +1,16 @@
+import { read, sort, type Nolytics } from '@data';
+import {
+    createStore, onLoading, setSuccess,
+} from './store';
+
+export const NolyticsStore = createNolyticsStore();
+
+function createNolyticsStore() {
+    const store = createStore<Nolytics>();
+    const subscribe = store.subscribe;
+
+    return {
+        subscribe,
+        read: () => onLoading(store, () => read().then((x) => setSuccess(store, sort(x)))),
+    };
+}
