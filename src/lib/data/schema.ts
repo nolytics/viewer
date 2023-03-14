85d797c70873ac9ad4db64c2c69b3311c0008cd4
freitzzz <1160907@isep.ipp.pt>
2023-03-14 21:48:35 +0000

    format

--- src/lib/data/schema.ts
+++ src/lib/data/schema.ts
@@ -3,7 +3,7 @@ type PageId = string;
 
 export interface Document {
 	_id: ObjectId;
-};
+}
 
 export interface Page extends Document {
 	name: string;
@@ -11,44 +11,44 @@ export interface Page extends Document {
 }
 
 export interface Hit extends Document {
-	pageId: PageId,
+	pageId: PageId;
 	client: Client;
 	metadata: Metadata;
-};
+}
 
 export interface Client {
 	anonymizedId: string;
 	country: string;
-};
+}
 
 export interface Metadata {
 	browser: string;
 	isMobile: boolean;
-};
+}
 
 export interface PageSummary {
 	hitsSummary: HitsSummary;
 	visitorsSummary: VisitorsSummary;
 	metadata: NolyticsMetadata;
-};
+}
 
 export type HitsSummary = Array<PageHitsSummary>;
 
 export interface PageHitsSummary {
 	pageId: PageId;
 	hitsCount: number;
-};
+}
 
 export interface VisitorsSummary {
 	mobileVisitorsCount: number;
 	nonMobileVisitorsCount: number;
 	mobileVisitorsPercentage: number;
 	nonMobileVisitorsPercentage: number;
-};
+}
 
 export interface NolyticsMetadata {
 	websiteUrl: string;
 	lastUpdatedMS: number;
-};
+}
 
 export type Nolytics = PageSummary;
