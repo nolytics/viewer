e65a703452ade859f0d12f498f2c6d17a69fd922
freitzzz <1160907@isep.ipp.pt>
2023-03-14 21:17:24 +0000

    sync nolytics schemas

--- src/lib/data/schema.ts
+++ src/lib/data/schema.ts
@@ -3,7 +3,7 @@ type PageId = string;
 
 export interface Document {
 	_id: ObjectId;
-}
+};
 
 export interface Page extends Document {
 	name: string;
@@ -11,39 +11,44 @@ export interface Page extends Document {
 }
 
 export interface Hit extends Document {
-	pageId: PageId;
+	pageId: PageId,
 	client: Client;
 	metadata: Metadata;
-}
+};
 
 export interface Client {
 	anonymizedId: string;
 	country: string;
-}
+};
 
 export interface Metadata {
 	browser: string;
 	isMobile: boolean;
-}
+};
 
 export interface PageSummary {
 	hitsSummary: HitsSummary;
 	visitorsSummary: VisitorsSummary;
-}
+	metadata: NolyticsMetadata;
+};
 
 export type HitsSummary = Array<PageHitsSummary>;
 
 export interface PageHitsSummary {
 	pageId: PageId;
-	hits: Array<Hit>;
 	hitsCount: number;
-}
+};
 
 export interface VisitorsSummary {
 	mobileVisitorsCount: number;
 	nonMobileVisitorsCount: number;
 	mobileVisitorsPercentage: number;
 	nonMobileVisitorsPercentage: number;
-}
+};
+
+export interface NolyticsMetadata {
+	websiteUrl: string;
+	lastUpdatedMS: number;
+};
 
 export type Nolytics = PageSummary;
