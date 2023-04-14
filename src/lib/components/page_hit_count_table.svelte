8c8df1572b8fac1cc0a4dc47b7102c0ff0edddc9
freitzzz <1160907@isep.ipp.pt>
2023-04-14 19:57:11 +0100

    feat(page-hit-count-table): highlight top pages based on a top hit count

--- src/lib/components/page_hit_count_table.svelte
+++ src/lib/components/page_hit_count_table.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
-	import type { HitsSummary } from '@data';
+	import type { HitsSummary, PageHitsSummary } from '@data';
+	import { onMobileBreakpoint } from '@media-query';
+	import { BreakpointStore } from '@stores';
+	import { onMount } from 'svelte';
 
 	export let hits: HitsSummary;
+	export let highlightTopPages: boolean = false;
+	export let highlightPagesCount: number = 10;
+
+	$: totalHitsCount = hits[0].hitsCount;
+	$: hitCountFillPercentage = (hit: PageHitsSummary) => (hit.hitsCount / totalHitsCount) * 100;
+
+	onMount(() => {
+		BreakpointStore.subscribe(() => {
+			if (!onMobileBreakpoint()) {
+				totalHitsCount = hits[0].hitsCount * 1.5;
+			} else {
+				totalHitsCount = hits[0].hitsCount;
+			}
+		});
+
+		BreakpointStore.listen();
+	});
 </script>
 
 <div class="flex w-full overflow-x-auto mt-5">
@@ -13,10 +33,19 @@
 			</tr>
 		</thead>
 		<tbody>
-			{#each hits as hit}
+			{#each hits as hit, index}
 				<tr>
 					<td>
-						{hit.pageId}
+						{#if highlightTopPages && index < highlightPagesCount}
+							<div
+								class="td-div"
+								style="--fill-percentage: {hitCountFillPercentage(hit).toFixed()}%;"
+							>
+								{hit.pageId}
+							</div>
+						{:else}
+							{hit.pageId}
+						{/if}
 					</td>
 					<td>
 						{hit.hitsCount}
@@ -26,3 +55,17 @@
 		</tbody>
 	</table>
 </div>
+
+<style>
+	.td-div {
+		color: #fff;
+
+		padding-left: 0.5%;
+
+		border-radius: 0.5ch;
+
+		background-color: rgb(255, 99, 132);
+		width: var(--fill-percentage);
+		line-height: 200%;
+	}
+</style>
